c1bf3fcb7222e1d95c7d8e940224a60376264397
LucaCasna <l.casna@gmail.com>
2015-10-07 15:04:14 +0200

    gestione attivita'

--- source/management_activities.php
+++ source/management_activities.php
@@ -1,101 +1,155 @@
-<!doctype html>
-<html lang="en" class="no-js">
+<html>
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>YPN | Attivita</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'>
+  <!--        CSS       -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/css_form/form_green.css" rel="stylesheet">
+  <link href="css/css_attivita/attivita.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/font-awesome.min.css" >
+  <link rel="stylesheet" href="css/font-awesome.min.css" >
+  <link rel="stylesheet" href="css/pace.css" >
+  <link rel="stylesheet" href="css/css_login/reset.css"> <!-- CSS reset -->
+  <link rel="stylesheet" href="css/css_login/style.css"> <!-- Gem style -->
+  <!--              -->
 
-	<link rel="stylesheet" href="css/css_management/reset.css"> <!-- CSS reset -->
-	<link rel="stylesheet" href="css/css_management/style.css"> <!-- Gem style -->
-	<script src="js/js_management/modernizr.js"></script> <!-- Modernizr -->
 
-	<title>Cross Reference Table | Cody</title>
+  <!-- JavaScript -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/pace.js"></script>
+  <!-- -->
 </head>
 <body>
+  <header role="banner" style="background-color:black;">
+    <div id="cd-logo"><a href="index.html"><a href="index.php" style="color:white;"><i  class="fa fa-chevron-left"></i> TORNA ALLA HOME</a></div>
 
-<section id="cd-table">
-	<header class="cd-table-column">
-		<h2>Features</h2>
-		<ul>
-			<li>Storage</li>
-			<li>People</li>
-			<li>Free trial</li>
-			<li>Support</li>
-			<li>Price</li>
-		</ul>
-	</header>
+    <nav class="main-nav">
+      <ul>
+        <!-- inser more links here -->
+        <li><a class="cd-signin" href="#0" style="background-color:rgb(50,72,31);">Sign in</a></li>
+        <li><a class="cd-signup" href="#0" style="background-color:rgb(149,59,69);">Sign up</a></li>
+      </ul>
+    </nav>
+  </header>
 
-	<div class="cd-table-container">
-		<div class="cd-table-wrapper">
+  <div class="cd-user-modal"> <!-- this is the entire modal form, including the background -->
+    <div class="cd-user-modal-container"> <!-- this is the container wrapper -->
+      <ul class="cd-switcher">
+        <li><a href="#0"  >Sign in</a></li>
+        <li><a href="#0">New account</a></li>
+      </ul>
 
-			<div class="cd-table-column">
-				<h2>Plan 1</h2>
-				<ul>
-					<li>1 GB</li>
-					<li>2</li>
-					<li>30 days</li>
-					<li class="cd-unchecked"><span>No</span></li>
-					<li>$9.99</li>
-					<li><a class="cd-select" href="#0">Select</a></li>
-				</ul>
-			</div> <!-- cd-table-column -->
+      <div id="cd-login"> <!-- log in form -->
+        <form class="cd-form">
+          <p class="fieldset">
+            <label class="image-replace cd-email" for="signin-email">E-mail</label>
+            <input class="full-width has-padding has-border" id="signin-email" type="email" placeholder="E-mail">
+            <span class="cd-error-message">Error message here!</span>
+          </p>
 
-			<div class="cd-table-column">
-				<h2>Plan 2</h2>
-				<ul>
-					<li>2 GB</li>
-					<li>5</li>
-					<li>30 days</li>
-					<li class="cd-unchecked"><span>No</span></li>
-					<li>$19.99</li>
-					<li><a class="cd-select" href="#0">Select</a></li>
-				</ul>
-			</div> <!-- cd-table-column -->
+          <p class="fieldset">
+            <label class="image-replace cd-password" for="signin-password">Password</label>
+            <input class="full-width has-padding has-border" id="signin-password" type="text"  placeholder="Password">
+            <a href="#0" class="hide-password">Hide</a>
+            <span class="cd-error-message">Error message here!</span>
+          </p>
 
-			<div class="cd-table-column">
-				<h2>Plan 3</h2>
-				<ul>
-					<li>5 GB</li>
-					<li>10</li>
-					<li>30 days</li>
-					<li class="cd-checked"><span>Yes</span></li>
-					<li>$29.99</li>
-					<li><a class="cd-select" href="#0">Select</a></li>
-				</ul>
-			</div> <!-- cd-table-column -->
+          <p class="fieldset">
+            <input type="checkbox" id="remember-me" checked>
+            <label for="remember-me">Remember me</label>
+          </p>
 
-			<div class="cd-table-column">
-				<h2>Plan 4</h2>
-				<ul>
-					<li>10 GB</li>
-					<li>20</li>
-					<li>30 days</li>
-					<li class="cd-checked"><span>Yes</span></li>
-					<li>$39.99</li>
-					<li><a class="cd-select" href="#0">Select</a></li>
-				</ul>
-			</div> <!-- cd-table-column -->
+          <p class="fieldset">
+            <input class="full-width" type="submit" value="Login">
+          </p>
+        </form>
 
-			<div class="cd-table-column">
-				<h2>Plan 5</h2>
-				<ul>
-					<li>20 GB</li>
-					<li>Unlimited</li>
-					<li>30 days</li>
-					<li class="cd-checked"><span>Yes</span></li>
-					<li>$49.99</li>
-					<li><a class="cd-select" href="#0">Select</a></li>
-				</ul>
-			</div> <!-- cd-table-column -->
+        <p class="cd-form-bottom-message"><a href="#0">Forgot your password?</a></p>
+        <!-- <a href="#0" class="cd-close-form">Close</a> -->
+      </div> <!-- cd-login -->
 
-		</div> <!-- cd-table-wrapper -->
-	</div> <!-- cd-table-container -->
+      <div id="cd-signup"> <!-- sign up form -->
+        <form class="cd-form">
+          <p class="fieldset">
+            <label class="image-replace cd-username" for="signup-username">Username</label>
+            <input class="full-width has-padding has-border" id="signup-username" type="text" placeholder="Username">
+            <span class="cd-error-message">Error message here!</span>
+          </p>
 
-	<em class="cd-scroll-right"></em>
-</section> <!-- cd-table -->
+          <p class="fieldset">
+            <label class="image-replace cd-email" for="signup-email">E-mail</label>
+            <input class="full-width has-padding has-border" id="signup-email" type="email" placeholder="E-mail">
+            <span class="cd-error-message">Error message here!</span>
+          </p>
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="js/js_management/main.js"></script> <!-- Gem jQuery -->
+          <p class="fieldset">
+            <label class="image-replace cd-password" for="signup-password">Password</label>
+            <input class="full-width has-padding has-border" id="signup-password" type="text"  placeholder="Password">
+            <a href="#0" class="hide-password">Hide</a>
+            <span class="cd-error-message">Error message here!</span>
+          </p>
+
+          <p class="fieldset">
+            <input type="checkbox" id="accept-terms">
+            <label for="accept-terms">I agree to the <a href="#0">Terms</a></label>
+          </p>
+
+          <p class="fieldset">
+            <input class="full-width has-padding" type="submit" value="Create account">
+          </p>
+        </form>
+
+        <!-- <a href="#0" class="cd-close-form">Close</a> -->
+      </div> <!-- cd-signup -->
+
+      <div id="cd-reset-password"> <!-- reset password form -->
+        <p class="cd-form-message">Lost your password? Please enter your email address. You will receive a link to create a new password.</p>
+
+        <form class="cd-form">
+          <p class="fieldset">
+            <label class="image-replace cd-email" for="reset-email">E-mail</label>
+            <input class="full-width has-padding has-border" id="reset-email" type="email" placeholder="E-mail">
+            <span class="cd-error-message">Error message here!</span>
+          </p>
+
+          <p class="fieldset">
+            <input class="full-width has-padding" type="submit" value="Reset password">
+          </p>
+        </form>
+
+        <p class="cd-form-bottom-message"><a href="#0">Back to log-in</a></p>
+      </div> <!-- cd-reset-password -->
+      <a href="#0" class="cd-close-form">Close</a>
+    </div> <!-- cd-user-modal-container -->
+  </div> <!-- cd-user-modal -->
+  <div class="subheader" >
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 nopadding" style="height:100px">
+      <!--<div class="col-lg-3 col-md-3 col-sm-3 col-xs-9">
+          <img src="images/img-menu-small.jpg" style="height:50px" alt="Logo"></a>
+      </div>-->
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" >
+          <a style="color:white;">ATTIVITA</a>
+      </div>
+
+    </div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 nopadding" style="background-color:white; padding:0px; height:100px" align="center" >
+        <a>GESTISCI LE TUE ATTIVITA'</a>
+    </div>
+  </div>
+
+  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <?php
+      include "management_content.php";
+
+    ?>
+  </div>
+
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="js/js_login/main.js"></script> <!-- Gem jQuery -->
 </body>
 </html>
---
+++ source/management_content.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/css_management/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/css_management/style.css"> <!-- Gem style -->
+	<script src="js/js_management/modernizr.js"></script> <!-- Modernizr -->
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+	<title>Cross Reference Table | Cody</title>
+</head>
+<body>
+
+<section id="cd-table">
+	<header class="cd-table-column">
+		<h2>Attivita'</h2>
+		<ul>
+			<li>Localita'</li>
+			<li>Foto Copertina</li>
+			<li>Ultima Modifica</li>
+			<!--<li>Modifica</li>
+			<li>Elimina</li>-->
+		</ul>
+	</header>
+
+	<div class="cd-table-container">
+		<div class="cd-table-wrapper">
+
+			<div class="cd-table-column">
+				<h2>Nome Attivita 1</h2>
+				<ul>
+					<li>nome località</li>
+					<li>url_foto.jpg</li>
+					<li>02/10/2015</li>
+
+					<li><a class="cd-select" href="#0" style="color:blue;"><i class="fa fa-pencil-square-o"></i></a></li>
+          <li><a class="cd-select" href="#0" style="color:red;"><i class="fa fa-times"></i></a></li>
+				</ul>
+			</div> <!-- cd-table-column -->
+
+			<div class="cd-table-column">
+				<h2>Nome Attivita 2</h2>
+        <ul>
+          <li>nome località</li>
+          <li>url_foto.jpg</li>
+          <li>02/10/2015</li>
+
+          <li><a class="cd-select" href="#0" style="color:blue;"><i class="fa fa-pencil-square-o"></i></a></li>
+          <li><a class="cd-select" href="#0" style="color:red;"><i class="fa fa-times"></i></a></li>
+        </ul>
+			</div> <!-- cd-table-column -->
+
+			<div class="cd-table-column">
+				<h2>Nome Attivita 3</h2>
+        <ul>
+					<li>nome località</li>
+					<li>url_foto.jpg</li>
+					<li>02/10/2015</li>
+
+					<li><a class="cd-select" href="#0" style="color:blue;"><i class="fa fa-pencil-square-o"></i></a></li>
+          <li><a class="cd-select" href="#0" style="color:red;"><i class="fa fa-times"></i></a></li>
+				</ul>
+			</div> <!-- cd-table-column -->
+
+			<div class="cd-table-column">
+				<h2>Nome Attivita 4</h2>
+        <ul>
+					<li>nome località</li>
+					<li>url_foto.jpg</li>
+					<li>02/10/2015</li>
+
+					<li><a class="cd-select" href="#0" style="color:blue;"><i class="fa fa-pencil-square-o"></i></a></li>
+          <li><a class="cd-select" href="#0" style="color:red;"><i class="fa fa-times"></i></a></li>
+				</ul>
+			</div> <!-- cd-table-column -->
+
+			<div class="cd-table-column">
+				<h2>Nome Attivita 5</h2>
+        <ul>
+					<li>nome località</li>
+					<li>url_foto.jpg</li>
+					<li>02/10/2015</li>
+
+					<li><a class="cd-select" href="#0" style="color:blue;"><i class="fa fa-pencil-square-o"></i></a></li>
+          <li><a class="cd-select" href="#0" style="color:red;"><i class="fa fa-times"></i></a></li>
+				</ul>
+			</div> <!-- cd-table-column -->
+
+		</div> <!-- cd-table-wrapper -->
+	</div> <!-- cd-table-container -->
+
+	<em class="cd-scroll-right"></em>
+</section> <!-- cd-table -->
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/js_management/main.js"></script> <!-- Gem jQuery -->
+</body>
+</html>

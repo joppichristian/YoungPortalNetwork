c3741d08a63c9290ada0c31554b4c4722c383fcb
joppichristian <joppi94@gmail.com>
2015-10-13 21:34:08 +0200

    gallery dinamica

--- source/gallery.php
+++ source/gallery.php
@@ -1,213 +1,98 @@
-<script type="text/javascript" src="js/jssor.slider.min.js"></script>
-    <script>
-        jssor_slider1_starter = function (containerId) {
 
-            var _SlideshowTransitions = [
-            //Fade in L
-                {$Duration: 1200, x: 0.3, $During: { $Left: [0.3, 0.7] }, $Easing: { $Left: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 }
-            //Fade out R
-                , { $Duration: 1200, x: -0.3, $SlideOut: true, $Easing: { $Left: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 }
-            //Fade in R
-                , { $Duration: 1200, x: -0.3, $During: { $Left: [0.3, 0.7] }, $Easing: { $Left: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 }
-            //Fade out L
-                , { $Duration: 1200, x: 0.3, $SlideOut: true, $Easing: { $Left: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 }
+<style>
+a:hover{
+  text-decoration: none;
+}
+.carousel {
+	margin-bottom: 0;
+	padding: 0 40px 30px 40px;
+}
 
-            //Fade in T
-                , { $Duration: 1200, y: 0.3, $During: { $Top: [0.3, 0.7] }, $Easing: { $Top: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2, $Outside: true }
-            //Fade out B
-                , { $Duration: 1200, y: -0.3, $SlideOut: true, $Easing: { $Top: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2, $Outside: true }
-            //Fade in B
-                , { $Duration: 1200, y: -0.3, $During: { $Top: [0.3, 0.7] }, $Easing: { $Top: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 }
-            //Fade out T
-                , { $Duration: 1200, y: 0.3, $SlideOut: true, $Easing: { $Top: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 }
+.carousel-control {
+	left: -12px;
+  background-color: rgba(50, 72, 31, 0.6);
+}
+.carousel-control.right {
+	right: -12px;
+}
 
-            //Fade in LR
-                , { $Duration: 1200, x: 0.3, $Cols: 2, $During: { $Left: [0.3, 0.7] }, $ChessMode: { $Column: 3 }, $Easing: { $Left: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2, $Outside: true }
-            //Fade out LR
-                , { $Duration: 1200, x: 0.3, $Cols: 2, $SlideOut: true, $ChessMode: { $Column: 3 }, $Easing: { $Left: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2, $Outside: true }
-            //Fade in TB
-                , { $Duration: 1200, y: 0.3, $Rows: 2, $During: { $Top: [0.3, 0.7] }, $ChessMode: { $Row: 12 }, $Easing: { $Top: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 }
-            //Fade out TB
-                , { $Duration: 1200, y: 0.3, $Rows: 2, $SlideOut: true, $ChessMode: { $Row: 12 }, $Easing: { $Top: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 }
+.carousel-indicators {
+	right: 50%;
+	top: auto;
+	bottom: 0px;
+	margin-right: -19px;
+}
 
-            //Fade in LR Chess
-                , { $Duration: 1200, y: 0.3, $Cols: 2, $During: { $Top: [0.3, 0.7] }, $ChessMode: { $Column: 12 }, $Easing: { $Top: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2, $Outside: true }
-            //Fade out LR Chess
-                , { $Duration: 1200, y: -0.3, $Cols: 2, $SlideOut: true, $ChessMode: { $Column: 12 }, $Easing: { $Top: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 }
-            //Fade in TB Chess
-                , { $Duration: 1200, x: 0.3, $Rows: 2, $During: { $Left: [0.3, 0.7] }, $ChessMode: { $Row: 3 }, $Easing: { $Left: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2, $Outside: true }
-            //Fade out TB Chess
-                , { $Duration: 1200, x: -0.3, $Rows: 2, $SlideOut: true, $ChessMode: { $Row: 3 }, $Easing: { $Left: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 }
+.carousel-indicators li {
+	background-color: rgba(50, 72, 31, 0.6);
+}
 
-            //Fade in Corners
-                , { $Duration: 1200, x: 0.3, y: 0.3, $Cols: 2, $Rows: 2, $During: { $Left: [0.3, 0.7], $Top: [0.3, 0.7] }, $ChessMode: { $Column: 3, $Row: 12 }, $Easing: { $Left: $JssorEasing$.$EaseInCubic, $Top: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2, $Outside: true }
-            //Fade out Corners
-                , { $Duration: 1200, x: 0.3, y: 0.3, $Cols: 2, $Rows: 2, $During: { $Left: [0.3, 0.7], $Top: [0.3, 0.7] }, $SlideOut: true, $ChessMode: { $Column: 3, $Row: 12 }, $Easing: { $Left: $JssorEasing$.$EaseInCubic, $Top: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2, $Outside: true }
+.carousel-indicators .active {
+background-color: rgb(50, 72, 31);
+}
+</style>
+<div id="myCarousel" class="carousel slide">
+  <?php
+  $qry_a="SELECT * FROM MEDIA_ATTIVITA WHERE ATTIVITA_ID = '$id_attivita' AND TIPO='FOTO' ;";
+  $result_a = $mysqli->query($qry_a);
+  $indice = 0;
+  ?>
+   <ol class="carousel-indicators">
+     <?php while($row_a = $result_a->fetch_array())
+     {
+       if($indice == 0) { ?>
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+     <?php
+   } else {
+     ?>
+     <li data-target="#myCarousel" data-slide-to="<?php echo $indice;?>"></li>
+     <?php
+     }
+     $indice = $indice +1;
+   }
+     ?>
 
-            //Fade Clip in H
-                , { $Duration: 1200, $Delay: 20, $Clip: 3, $Assembly: 260, $Easing: { $Clip: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 }
-            //Fade Clip out H
-                , { $Duration: 1200, $Delay: 20, $Clip: 3, $SlideOut: true, $Assembly: 260, $Easing: { $Clip: $JssorEasing$.$EaseOutCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 }
-            //Fade Clip in V
-                , { $Duration: 1200, $Delay: 20, $Clip: 12, $Assembly: 260, $Easing: { $Clip: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 }
-            //Fade Clip out V
-                , { $Duration: 1200, $Delay: 20, $Clip: 12, $SlideOut: true, $Assembly: 260, $Easing: { $Clip: $JssorEasing$.$EaseOutCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 }
-                ];
+   </ol>
 
-            var options = {
-                $AutoPlay: false,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
-                $AutoPlayInterval: 20000,                            //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
-                $PauseOnHover: 1,                                   //[Optional] Whether to pause when mouse over if a slider is auto playing, 0 no pause, 1 pause for desktop, 2 pause for touch device, 3 pause for desktop and touch device, 4 freeze for desktop, 8 freeze for touch device, 12 freeze for desktop and touch device, default value is 1
-                $DragOrientation: 3,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
-                $ArrowKeyNavigation: true,   			            //[Optional] Allows keyboard (arrow key) navigation or not, default value is false
-                $SlideDuration: 800,                                //Specifies default duration (swipe) for slide in milliseconds
+   <!-- Carousel items -->
+   <div class="carousel-inner">
 
-                $SlideshowOptions: {                                //[Optional] Options to specify and enable slideshow or not
-                    $Class: $JssorSlideshowRunner$,                 //[Required] Class to create instance of slideshow
-                    $Transitions: _SlideshowTransitions,            //[Required] An array of slideshow transitions to play slideshow
-                    $TransitionsOrder: 1,                           //[Optional] The way to choose transition to play slide, 1 Sequence, 0 Random
-                    $ShowLink: true                                 //[Optional] Whether to bring slide link on top of the slider when slideshow is running, default value is false
-                },
+     <?php
+     $indice = 0;
+     $result_a = $mysqli->query($qry_a);
+     while($row_a = $result_a->fetch_array())
+     {
+       if($indice == 0) { ?>
+         <div class="item active">
+           <img src="<?php echo $row_a['URL']; ?>" style="max-width:100%;"/>
+         </div>
+     <?php
+   } else {
+     ?>
+     <div class="item">
+       <img src="<?php echo $row_a['URL']; ?>" style="max-width:100%;"/>
+     </div>
+     <?php
+     }
+     $indice = $indice +1;
+   }
+     ?>
 
-                $ArrowNavigatorOptions: {                           //[Optional] Options to specify and enable arrow navigator or not
-                    $Class: $JssorArrowNavigator$,                  //[Requried] Class to create arrow navigator instance
-                    $ChanceToShow: 1                                //[Required] 0 Never, 1 Mouse Over, 2 Always
-                },
+   </div><!--/carousel-inner-->
 
-                $ThumbnailNavigatorOptions: {                       //[Optional] Options to specify and enable thumbnail navigator or not
-                    $Class: $JssorThumbnailNavigator$,              //[Required] Class to create thumbnail navigator instance
-                    $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
-                    $ActionMode: 1,                                 //[Optional] 0 None, 1 act by click, 2 act by mouse hover, 3 both, default value is 1
-                    $SpacingX: 8,                                   //[Optional] Horizontal space between each thumbnail in pixel, default value is 0
-                    $DisplayPieces: 10,                             //[Optional] Number of pieces to display, default value is 1
-                    $ParkingPosition: 360                           //[Optional] The offset position to park thumbnail
-                }
-            };
-
-            var jssor_slider1 = new $JssorSlider$(containerId, options);
-            //responsive code begin
-            //you can remove responsive code if you don't want the slider scales while window resizes
-            function ScaleSlider() {
-                var parentWidth = jssor_slider1.$Elmt.parentNode.clientWidth;
-                if (parentWidth)
-                    jssor_slider1.$ScaleWidth(Math.max(Math.min(parentWidth, 800), 300));
-                else
-                    $Jssor$.$Delay(ScaleSlider, 30);
-            }
-
-            ScaleSlider();
-            $Jssor$.$AddEvent(window, "load", ScaleSlider);
-
-            $Jssor$.$AddEvent(window, "resize", $Jssor$.$WindowResizeFilter(window, ScaleSlider));
-            $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
-            //responsive code end
-        };
-    </script>
-    <!-- Jssor Slider Begin -->
-    <!-- You can move inline styles to css file or css block. -->
-    <div id="slider1_container" class="slider1_container"  >
-
-        <!-- Loading Screen -->
-        <div u="loading" class="loading">
-            <div class="first-div-loading">
-            </div>
-            <div class="second-div-loading">
-            </div>
-        </div>
-
-        <!-- Slides Container -->
-        <div u="slides" id="slides">
-		
-			<?php
-			$qry_a="SELECT * FROM MEDIA_ATTIVITA WHERE ATTIVITA_ID = '$id_attivita' AND TIPO='FOTO' ;";
-			$result_a = $mysqli->query($qry_a);
-			while($row_a = $result_a->fetch_array())
-			{
-				echo  $row_a['URL']; 
-			?>		
-				<div>
-					<img u="image" src="<?php echo $row_a['URL']; ?>" />
-					<img u="thumb" src="<?php echo $row_a['URL_THUMB']; ?>" />
-				</div>
-			<?php
-	        }		
-			?>	
-		 <!--
-            <div>
-                <img u="image" src="images/examples_gallery/alila/ciao.jpg" />
-                <img u="thumb" src="images/examples_gallery/alila/thumb-ciao.jpg" />
-            </div>
-            <div>
-                <img u="image" src="images/examples_gallery/alila/caio.jpg" />
-                <img u="thumb" src="images/examples_gallery/alila/thumb-caio.jpg" />
-            </div>
-            <div>
-                <img u="image" src="images/examples_gallery/alila/03.jpg" />
-                <img u="thumb" src="images/examples_gallery/alila/thumb-03.jpg" />
-            </div>
-            <div>
-                <img u="image" src="images/examples_gallery/alila/04.jpg" />
-                <img u="thumb" src="images/examples_gallery/alila/thumb-04.jpg" />
-            </div>
-            <div>
-                <img u="image" src="images/examples_gallery/alila/05.jpg" />
-                <img u="thumb" src="images/examples_gallery/alila/thumb-05.jpg" />
-            </div>
-            <div>
-                <img u="image" src="images/examples_gallery/alila/06.jpg" />
-                <img u="thumb" src="images/examples_gallery/alila/thumb-06.jpg" />
-            </div>
-            <div>
-                <img u="image" src="images/examples_gallery/alila/07.jpg" />
-                <img u="thumb" src="images/examples_gallery/alila/thumb-07.jpg" />
-            </div>
-            <div>
-                <img u="image" src="images/examples_gallery/alila/08.jpg" />
-                <img u="thumb" src="images/examples_gallery/alila/thumb-08.jpg" />
-            </div>
-            <div>
-                <img u="image" src="images/examples_gallery/alila/09.jpg" />
-                <img u="thumb" src="images/examples_gallery/alila/thumb-09.jpg" />
-            </div>
-            <div>
-                <img u="image" src="images/examples_gallery/alila/10.jpg" />
-                <img u="thumb" src="images/examples_gallery/alila/thumb-10.jpg" />
-            </div>
-
-            <div>
-                <img u="image" src="images/examples_gallery/alila/11.jpg" />
-                <img u="thumb" src="images/examples_gallery/alila/thumb-11.jpg" />
-            </div>
-            <div>
-                <img u="image" src="images/examples_gallery/alila/12.jpg" />
-                <img u="thumb" src="images/examples_gallery/alila/thumb-12.jpg" />
-            </div> -->
-        </div>
-
-        <!-- Arrow Navigator Skin Begin -->
-        <!-- Arrow Left -->
-      
-
-        <!-- Thumbnail Navigator Skin Begin -->
-        <div u="thumbnavigator" class="jssort01" style="position: absolute; width: 800px; height: 100px; left:0px; bottom: 0px;">
-            <!-- Thumbnail Item Skin Begin -->
-
-            <div u="slides" style="cursor: move;">
-                <div u="prototype" class="p" style="position: absolute; width: 72px; height: 72px; top: 0; left: 0;">
-                    <div class=w>
-                    	<div u="thumbnailtemplate" class="thumbnailtemplate"></div>
-                    </div>
-                    <div class=c>
-                    </div>
-                </div>
-            </div>
-            <!-- Thumbnail Item Skin End -->
-        </div>
-        <!-- Thumbnail Navigator Skin End -->
-        <a style="display: none" href="http://www.jssor.com">javascript</a>
-        <!-- Trigger -->
-        <script>
-            jssor_slider1_starter('slider1_container');
-        </script>
-    </div>
-    <!-- Jssor Slider End -->
+   <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+       <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+       <span class="sr-only">Previous</span>
+     </a>
+     <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+       <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+       <span class="sr-only">Next</span>
+     </a>
+   </div><!--/myCarousel-->
+   <script>
+   $(document).ready(function() {
+     $('#myCarousel').carousel({
+     interval: 1000
+   });
+   });
+   </script>

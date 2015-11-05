e7b850b06374ecc38b919b3bb3795e12175b2f95
joppichristian <joppi94@gmail.com>
2015-11-05 16:32:45 +0100

    fix some style bugs

--- source/header.php
+++ source/header.php
@@ -3,7 +3,7 @@
 if( strcmp( $testoIndietro , "#") == 0  ){			 
 ?>	
 	<div class="logo" style="float:left;"  >
-		<font style="color:rgb(50,72,31);">V A L L E  </font><font style="color:rgb(23,148,201);">D I   </font><font style="color:rgb(149,59,69);">C E M B R A</font>
+		<font style="color:rgb(180,183,34);">V A L L E  </font><font style="color:#008fd7;">D I   </font><font style="color:#eb8c06;">C E M B R A</font>
 	</div>
 <?php
 }else{
@@ -24,8 +24,8 @@ if( strcmp( $testoIndietro , "#") == 0  ){
 	  <?php
 	  }else{
 	  ?>		
-		  <li><a class="cd-signin" style="background-color:rgb(23,148,201);" href="#0">Accedi</a></li>
-		  <li><a class="cd-signup" style="background-color:rgb(149,59,69);" href="#0">Registrati</a></li>
+		  <li><a class="cd-signin" style="background-color:rgb(0,144,215);" href="#0">Accedi</a></li>
+		  <li><a class="cd-signup" style="background-color:rgb(234,140,6);" href="#0">Registrati</a></li>
 	  <?php
 	  }
 	  ?>

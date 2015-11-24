d32b04ac5271d173eeffe2c2aa26e1084d3d90ce
LucaCasna <l.casna@gmail.com>
2015-11-24 11:56:43 +0100

    cookie e privacy policy

--- source/addCurriculum.php
+++ source/addCurriculum.php
@@ -42,39 +42,55 @@ $testoIndietro = "TORNA INDIETRO";
 
   <script language="JavaScript" type="text/JavaScript">
 	function validateForm()
-	{	
-		var message = "ATTENZIONE:\n";
+	{
+    var message = "ATTENZIONE:\n";
+    var privacy = document.getElementById("privacy").checked;
+    if(privacy!=true){
+
+      $.alert({
+				title: 'Aggiungi Curriculum',
+				content: message+"Spunta la normativa sulla privacy per poter aggiungere il tuo Curriculum!",
+				theme: 'supervan',
+				animation:'RotateY',
+				 animationSpeed: 1000,
+				confirm: function (id) {
+
+				}
+				});
+        return false;
+    }else{
+
 		var campi   = "";
-		
-		var nome = document.getElementById("nome").value;		
-		var cognome = document.getElementById("cognome").value;				
+
+		var nome = document.getElementById("nome").value;
+		var cognome = document.getElementById("cognome").value;
 		var data  = document.getElementById("data").value;
 		var residenza  = document.getElementById("residenza").value;
 		var telefono  = document.getElementById("telefono").value;
 		var email  = document.getElementById("email").value;
-	 	 
+
 		if(nome==""){
-			campi = campi+" \n[nome] OBBLIGATORIO";			
+			campi = campi+" \n[nome] OBBLIGATORIO";
 		}
 		if(cognome==""){
-			campi = campi+" \n[cognome] OBBLIGATORIO";			
+			campi = campi+" \n[cognome] OBBLIGATORIO";
 		}
 		if(data==""){
-			campi = campi+" \n[data di nascita] OBBLIGATORIO";			
+			campi = campi+" \n[data di nascita] OBBLIGATORIO";
 		}
 		if(residenza==""){
-			campi = campi+" \n[residenza] OBBLIGATORIO";			
+			campi = campi+" \n[residenza] OBBLIGATORIO";
 		}
 		if(telefono==""){
-			campi = campi+" \n[telefono] OBBLIGATORIO";			
+			campi = campi+" \n[telefono] OBBLIGATORIO";
 		}
 		if(email==""){
-			campi = campi+" \n[email] OBBLIGATORIO";			
+			campi = campi+" \n[email] OBBLIGATORIO";
 		}
 		if(document.getElementById("file").value.length < 1) {
-		   campi = campi+" \n[immagine] OBBLIGATORIO";			
+		   campi = campi+" \n[immagine] OBBLIGATORIO";
 		}
-		
+
 		if(campi!=("")){
 			$.alert({
 				title: 'Aggiungi Curriculum',
@@ -83,18 +99,19 @@ $testoIndietro = "TORNA INDIETRO";
 				animation:'RotateY',
 				 animationSpeed: 1000,
 				confirm: function (id) {
-				 
-				}                                        
+
+				}
 				});
 			return false;
 		}
 		else
-		{		
+		{
 			document.submitForm.action = 'post-add-cv.php';
 			document.submitForm.submit();
-		}		
+		}
+    }
 	}
-  </script> 
+  </script>
 </head>
 <body>
   <header role="banner" style="background-color:black;">
@@ -219,10 +236,18 @@ $testoIndietro = "TORNA INDIETRO";
 			<input type="file" name="file" id="file" />
 			<p>N.B.: L'immagine verrà usata come foto profilo.</p>
 		</div>
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-top:2%;margin-bottom:2%;" >
+      <input id="privacy" type="checkbox" name="privacy" value="off">Accetto la <a href="//www.iubenda.com/privacy-policy/587389" class="iubenda-white iubenda-embed" title="Privacy Policy">Privacy Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src = "//cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+		<div>
+
+
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-top:2%;margin-bottom:2%;" >
 		  <button type="submit" value="Aggiugi" style="font-size: 25px;" >Aggiungi</button>
 		  <button type="reset" value="Cancella" style="font-size: 25px;">Cancella</button>
 		<div>
+
+
 
 	  </form>
   <?php
--- source/header.php
+++ source/header.php
@@ -1,7 +1,20 @@
 
-<?php	
-if( strcmp( $testoIndietro , "#") == 0  ){			 
-?>	
+<script type="text/javascript">
+	var _iub = _iub || [];
+	_iub.csConfiguration = {
+		cookiePolicyId: 587389,
+		siteId: 446959,
+		lang: "it"
+	};
+	(function (w, d) {
+		var loader = function () { var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src = "//cdn.iubenda.com/cookie_solution/iubenda_cs.js"; tag.parentNode.insertBefore(s, tag); };
+		if (w.addEventListener) { w.addEventListener("load", loader, false); } else if (w.attachEvent) { w.attachEvent("onload", loader); } else { w.onload = loader; }
+	})(window, document);
+</script>
+
+<?php
+if( strcmp( $testoIndietro , "#") == 0  ){
+?>
 	<div class="logo" style="float:left;"  >
 		<font style="color:rgb(180,183,34);">V A L L E  </font><font style="color:#008fd7;">D I   </font><font style="color:#eb8c06;">C E M B R A</font>
 	</div>
@@ -10,7 +23,7 @@ if( strcmp( $testoIndietro , "#") == 0  ){
 ?>
 	<div id="cd-logo"><a href="<?php echo $linkIndietro; ?>"><a href="<?php echo $linkIndietro; ?>"><i  class="fa fa-chevron-left"></i><?php echo $testoIndietro; ?></a></div>
 <?php
-} 
+}
 ?>
 
 <nav class="main-nav" >
@@ -18,12 +31,12 @@ if( strcmp( $testoIndietro , "#") == 0  ){
 	  <!-- inser more links here -->
 	  <?php
 	  if(utenteLoggato($mysqli) == true) {
-	  ?>  
+	  ?>
 		  <li style="color:#eb8c06;background-color: black; cursor: pointer;">Benvenuto, <?php echo $_SESSION['username']; ?></li>
 		  <li><a id="a-esci" href="private/logout.php" > Esci </a></li>
 	  <?php
 	  }else{
-	  ?>		
+	  ?>
 		  <li><a class="cd-signin" style="background-color:rgb(0,144,215);" href="#0">Accedi</a></li>
 		  <li><a class="cd-signup" style="background-color:rgb(234,140,6);" href="#0">Registrati</a></li>
 	  <?php
